db152424befcf6e84df252f090ca9b1d429bba8d
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2023-09-02 09:36:30 +0600

    first commit

---
+++ sctipt.js
@@ -0,0 +1,107 @@
+let loaddata = async () => {
+    let res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
+    let data = await res.json();
+    let tube = data.data
+    // console.log(tube)
+    displayphone(tube)
+}
+let section = document.getElementById('section');
+let displayphone = (data) => {
+    data.forEach(element => {
+        let div = document.createElement("div");
+        div.classList = `m-2`
+        div.innerHTML = `
+        <button onclick="handeltube('${element.category_id}')" class="btn">${element.category}</button>
+        `
+        section.appendChild(div)
+        // console.log(element.category)
+    });
+
+}
+let handeltube = async (categoryid) => {
+    let res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryid}`)
+    let data = await res.json();
+    // console.log(data.data)
+    if (data.data.length === 0) {
+        console.log("clcked here")
+        window.location.href = "nodata.html";
+    }
+
+
+    else {
+        console.log("data havent")
+    }
+    let cardcontainer = document.getElementById('card-container')
+    cardcontainer.innerHTML = " ";
+    data.data.forEach(element => {
+
+        let mind = element.others.posted_date % (60 * 60);
+        let minutes = Math.floor(mind / 60);
+        function toHoursAndMinutes(totalMinutes) {
+            const hours = Math.floor(totalMinutes / 60);
+            const minutes = totalMinutes % 60;
+            return hours + "hrs" + " " + minutes + "min ago";
+        }
+        let hour = toHoursAndMinutes(minutes)
+        console.log(hour)
+        let div1 = document.createElement('div');
+        let div2 = document.createElement('div');
+        div1.classList = `card bg-base-100 shadow-xl `
+        div1.innerHTML = `
+        <div class="relative">
+        <figure><img class="h-52" src=${element.thumbnail
+            } alt="Shoes" /></figure>
+        <div class="absolute bottom-0 right-0 text-white bg-gray-700">${hour}</div>
+        </div>
+        `
+        element.authors.forEach(authordata => {
+            div2.innerHTML = `
+             <div class="card  ">
+             <div class="card-body ">
+               <div class="card-actions ">
+               <img class="h-10 w-10 rounded-full " src=${authordata.profile_picture}
+               alt="Shoes" />
+               <p class="text-xl">${element.title}</p>
+               </div>
+               
+               <div class="card-actions">
+               <h3>${authordata.profile_name}
+               </h3>
+               <div class="w-10" >${authordata.verified == true ? " <img src='./image.png'>" : ''}</div>
+               </div>
+               <h4> ${element.others.views}</h4>
+             </div>
+           </div>
+            `
+
+            //  let fortnam=sort(element);
+            //  console.log(fortnam)
+        });
+
+        cardcontainer.appendChild(div1)
+        div1.appendChild(div2)
+
+    })
+
+}
+
+let blog = () => {
+    let blogpass = document.getElementById('blog')
+    window.location.href = "blog.html";
+}
+
+
+// let sort=(element)=>{
+//     console.log(element.others.views)
+//   let number=parseFloat(element.others.views).split("k")[0];
+//   number.sort((a,b)=>{ 
+//    return b-a
+//   })
+//   console.log()
+// }
+
+
+
+loaddata()
+
+
