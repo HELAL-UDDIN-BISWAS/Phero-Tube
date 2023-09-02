79776189a013f18c48c636b90a4ec02d8f2bbe72
HELAL-UDDIN-BISWAS <mdhelal6775@gmail.com>
2023-09-02 12:27:34 +0600

    minig full coment

--- sctipt.js
+++ sctipt.js
@@ -8,59 +8,72 @@ let loaddata = async () => {
 let section = document.getElementById('section');
 let displayphone = (data) => {
     data.forEach(element => {
-        let div = document.createElement("div");
-        div.classList = `m-2`
-        div.innerHTML = `
-        <button onclick="handeltube('${element.category_id}')" class="btn">${element.category}</button>
+        let div1 = document.createElement("div");
+        div1.classList = `m-2`
+        div1.innerHTML = `
+        <button onclick="handeltube('${element.category_id}')" class="btn"   >${element.category}</button>
         `
-        section.appendChild(div)
+        section.appendChild(div1)
         // console.log(element.category)
     });
-
 }
 let handeltube = async (categoryid) => {
     let res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryid}`)
     let data = await res.json();
     // console.log(data.data)
+    // =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+let cardcontainer = document.getElementById('card-container')
+cardcontainer.innerHTML = " ";
+
+
     if (data.data.length === 0) {
-        console.log("clcked here")
-        window.location.href = "nodata.html";
+        // let cardcontainer1 = document.getElementById('card-container1');
+        cardcontainer.innerHTML = " ";
+        let div= document.createElement('div');
+        div.innerHTML = " ";
+        div.classList=`
+        text-center items-center justify-center flex
+        `
+        div.innerHTML = `
+        <div class="card w-96 bg-base-100 mt-6 h-screen mt-8">
+        <figure><img class="w-60" src="./Icon.png" alt="Shoes" /></figure>
+        <div class="card-body">
+            <h2 class="w- text-2xl">Oops!! Sorry, There is no content here</h2>
+        </div>
+      </div>
+      `
+        cardcontainer.appendChild(div)
+        console.log(cardcontainer)
+        // window.location.href = "nodata.html";
     }
-
-
     else {
-        console.log("data havent")
-    }
-    let cardcontainer = document.getElementById('card-container')
-    cardcontainer.innerHTML = " ";
-    data.data.forEach(element => {
+        data.data.forEach(element => {
 
-        let mind = element.others.posted_date % (60 * 60);
-        let minutes = Math.floor(mind / 60);
-        function toHoursAndMinutes(totalMinutes) {
-            const hours = Math.floor(totalMinutes / 60);
-            const minutes = totalMinutes % 60;
-            return hours + "hrs" + " " + minutes + "min ago";
-        }
-        let hour = toHoursAndMinutes(minutes)
-        console.log(hour)
-        let div1 = document.createElement('div');
-        let div2 = document.createElement('div');
-        div1.classList = `card bg-base-100 shadow-xl `
-        div1.innerHTML = `
+            let mind = element.others.posted_date % (60 * 60);
+            let minutes = Math.floor(mind / 60);
+            function toHoursAndMinutes(totalMinutes) {
+                const hours = Math.floor(totalMinutes / 60);
+                const minutes = totalMinutes % 60;
+                return hours + "hrs" + " " + minutes + "min ago";
+            }
+            let hour = toHoursAndMinutes(minutes)
+            console.log(hour)
+            let div1 = document.createElement('div');
+            let div2 = document.createElement('div');
+            div1.classList = `card bg-base-100 shadow-xl `
+            div1.innerHTML = `
         <div class="relative">
         <figure><img class="h-52" src=${element.thumbnail
-            } alt="Shoes" /></figure>
+                } alt="Shoes" /></figure>
         <div class="absolute bottom-0 right-0 text-white bg-gray-700">${hour}</div>
         </div>
         `
-        element.authors.forEach(authordata => {
-            div2.innerHTML = `
+            element.authors.forEach(authordata => {
+                div2.innerHTML = `
              <div class="card  ">
              <div class="card-body ">
                <div class="card-actions ">
-               <img class="h-10 w-10 rounded-full " src=${authordata.profile_picture}
-               alt="Shoes" />
+               <img class="h-10 w-10 rounded-full " src=${authordata.profile_picture} alt="Shoes" />
                <p class="text-xl">${element.title}</p>
                </div>
                
@@ -73,35 +86,26 @@ let handeltube = async (categoryid) => {
              </div>
            </div>
             `
-
-            //  let fortnam=sort(element);
-            //  console.log(fortnam)
-        });
-
-        cardcontainer.appendChild(div1)
-        div1.appendChild(div2)
-
-    })
-
+                //  let fortnam=sort(element);
+                //  console.log(fortnam)
+            });
+            cardcontainer.appendChild(div1)
+            div1.appendChild(div2)
+        })
+    }
+    // let sort=(element)=>{
+    //     console.log(element.others.views)
+    //   let number=parseFloat(element.others.views).split("k")[0];
+    //   number.sort((a,b)=>{ 
+    //    return b-a
+    //   })
+    //   console.log()
+    // }
 }
-
 let blog = () => {
     let blogpass = document.getElementById('blog')
     window.location.href = "blog.html";
 }
-
-
-// let sort=(element)=>{
-//     console.log(element.others.views)
-//   let number=parseFloat(element.others.views).split("k")[0];
-//   number.sort((a,b)=>{ 
-//    return b-a
-//   })
-//   console.log()
-// }
-
-
-
 loaddata()
 
 
